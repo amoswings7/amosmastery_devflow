532f9bcc7676ec05928a155b0a455ad95ba01c0d
amoswings7 <amoswings7@gmail.com>
2024-11-06 13:58:33 +0200

    Assets & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-violet-700">
+    <h1 className="text-3xl font-black text-primary-500">
       Welcome to the world of Next.js 15
     </h1>
   );
